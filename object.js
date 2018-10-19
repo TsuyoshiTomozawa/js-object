8a9035c953650e512dc3d237aeace6f0f98727ba
TsuyoshiTomozawa <sphdm244@gmail.com>
2018-10-19 10:18:34 +0900

    first commit

---
+++ object.js
@@ -0,0 +1,29 @@
+(function() {
+	'use strict';
+	var game = {
+		startTime: null,
+		displayArea: document.getElementById('display-area'),
+		start: function() {
+			game.startTime = new Date().getTime();
+			document.body.onkeypress = game.stop;
+			console.log('start');
+		},
+		stop: function() {
+			var currentTime = new Date().getTime();
+			var seconds = (currentTime - game.startTime) / 1000;
+			if(9.5 <= seconds && seconds <= 10.5) {
+				game.displayArea.innerText = seconds + '秒でした。すごい。';
+			} else {
+				game.displayArea.innerText = seconds + '秒でした。残念。';
+			}
+
+			console.log('stop');
+		}
+	}
+
+	if(confirm('OKを押して10秒だと思ったら何かキーを押して下さい')) {
+		//TODO スタート
+		game.start();
+		
+	}
+})();
